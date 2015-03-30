f7baf9b5a69c227a2485f8d46c8fb44041641eee
SirTobi <code.databyte@gmail.com>
2015-03-30 11:13:21 +0200

    added serialization for std::forward_list

---
+++ include/xnet/serialization/types/forward_list.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#ifndef _XNET_SERIALIZATION_TYPE_FORWARDLIST_HPP
+#define _XNET_SERIALIZATION_TYPE_FORWARDLIST_HPP
+
+
+#include <forward_list>
+#include "../serialization.hpp"
+#include "detail/serialize_sequence.hpp"
+
+namespace xnet {
+	namespace serialization {
+		namespace detail {
+			template<typename Sink, typename T>
+			void split_serialize_forward_list(serializer<Sink>& s, const std::forward_list<T>& v)
+			{
+				detail::save_sequence(s, v.begin(), v.end(), std::distance(v.cbegin(), v.cend()), nullptr);
+			}
+
+			template<typename Source, typename T>
+			void split_serialize_forward_list(deserializer<Source>& s, std::forward_list<T>& v)
+			{
+				auto size = s.begin_sequence_load(nullptr);
+				v.clear();
+				auto it = v.before_begin();
+				while(size--)
+				{
+					it = v.emplace_after(it);
+					s >> *it;
+				}
+				s.end_sequence_load(nullptr);
+			}
+		}
+
+
+		template<typename S, typename T>
+		void serialize(S& s, std::forward_list<T>& v)
+		{
+			s.current_type("std::forward_list");
+			detail::split_serialize_forward_list(s, v);
+		}
+	}
+}
+
+
+#endif
